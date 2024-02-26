b95d226498035fac0571108dc594629623dc8508
bruno-sl15 <bruno.silvaleite15@gmail.com>
2024-02-26 17:47:22 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Header/>
         <Sidebar/>
+        <Footer/>
       </div>
     );
   }
